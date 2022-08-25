7c6328a526f560108f10ba11ee9cbcc32f908a73
Joshua Barretto <joshua.s.barretto@gmail.com>
2022-08-25 19:14:41 +0100

    Fixed incorrect swaying in shadow vertex

--- shaders/blended_depth_postpass_vertex.glsl
+++ shaders/blended_depth_postpass_vertex.glsl
@@ -5,26 +5,21 @@ uniform mat4 projectionMatrix;
 varying vec2 diffuseMapUV;
 varying float alphaPassthrough;
 
-uniform float osg_SimulationTime;
 uniform mat4 osg_ViewMatrixInverse;
 uniform mat4 osg_ModelViewMatrix;
 uniform sampler2D diffuseMap;
 
 #include "vertexcolors.glsl"
+#include "sway.glsl"
 
 void main()
 {
     diffuseMapUV = (gl_TextureMatrix[0] * gl_MultiTexCoord0).xy;
 
-    vec4 pos = gl_Vertex;
-    if (texture2D(diffuseMap, diffuseMapUV).a < 0.2 && dot(gl_FrontMaterial.emission.rgb, vec3(1)) == 0) {
-        //vec3 wpos = (osg_ViewMatrixInverse * osg_ModelViewMatrix * gl_Vertex).xyz;
-        pos.xyz += sin(osg_SimulationTime + gl_Vertex.yzx * 0.01)
-            * 0.01
-            * gl_Vertex.z
-        ;
-    }
-    gl_Position = projectionMatrix * (gl_ModelViewMatrix * pos);
+    vec4 modelPos = gl_Vertex;
+    float leafiness;
+    doSway(diffuseMap, diffuseMapUV, modelPos.xyz, leafiness);
+    gl_Position = projectionMatrix * (gl_ModelViewMatrix * modelPos);
 
     if (colorMode == 2)
         alphaPassthrough = gl_Color.a;
