1da6bc3725416c6ada0ca48c758a3d3d85775862
Joshua Barretto <joshua.s.barretto@gmail.com>
2022-12-08 23:27:06 +0000

    Disabled swaying in interiors

--- shaders/sway.glsl
+++ shaders/sway.glsl
@@ -3,7 +3,11 @@
 uniform float osg_SimulationTime;
 
 void doSway(sampler2D diffuseMap, vec2 diffuseMapUV, inout vec3 modelPos, inout float leafiness) {
-    if (texture2D(diffuseMap, diffuseMapUV).a < 0.2 && dot(gl_FrontMaterial.emission.rgb, vec3(1.0)) == 0.0) {
+    if (texture2D(diffuseMap, diffuseMapUV).a < 0.05
+        && dot(gl_FrontMaterial.emission.rgb, vec3(1.0)) == 0.0
+        //&& abs(abs(gl_Normal.z) - 0.5) < 0.4
+        // Only enable swaying in exteriors, for now
+        && step(0.0, (osg_ViewMatrixInverse * vec4(lcalcPosition(0), 0.0)).y) == 0.0) {
         if (WIND_AMPLITUDE > 0.0) {
             modelPos.xyz += sin(osg_SimulationTime * (WIND_AMPLITUDE + 1.0) * 1.5 + gl_Vertex.yzx * 0.01)
                 * 0.01
