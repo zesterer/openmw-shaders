a48bb4acf0824cc3668b117a488ba6f331e80401
Joshua Barretto <joshua.s.barretto@gmail.com>
2022-12-13 19:41:33 +0000

    Fixed broken shader features

--- shaders/blended_depth_postpass_vertex.glsl
+++ shaders/blended_depth_postpass_vertex.glsl
@@ -1,4 +1,12 @@
-#version 140
+#version 120
+
+#if @useUBO
+    #extension GL_ARB_uniform_buffer_object : require
+#endif
+
+#if @useGPUShader4
+    #extension GL_EXT_gpu_shader4: require
+#endif
 
 uniform mat4 projectionMatrix;
 
