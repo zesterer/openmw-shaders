3428a4fe8a5c582a8e15379f662e4a0d884fd6e1
Joshua Barretto <joshua.s.barretto@gmail.com>
2023-01-24 20:11:16 +0000

    Added support for soft shadows

--- shaders/shadows_fragment.glsl
+++ shaders/shadows_fragment.glsl
@@ -4,7 +4,7 @@
     uniform float maximumShadowMapDistance;
     uniform float shadowFadeStart;
     @foreach shadow_texture_unit_index @shadow_texture_unit_list
-        uniform sampler2DShadow shadowTexture@shadow_texture_unit_index;
+        uniform sampler2D shadowTexture@shadow_texture_unit_index;
         varying vec4 shadowSpaceCoords@shadow_texture_unit_index;
 
 #if @perspectiveShadowMaps
@@ -12,6 +12,78 @@
 #endif
     @endforeach
 #endif // SHADOWS
+
+#if SHADOWS
+const float lightSizeFactor = 0.05;
+const float nearPlane = 0.4;
+const float maxSearchDistance = 0.01;
+const float maxFilterRadius = 0.01;
+const int shadowSampleCount = 16;
+
+float interleavedGradientNoise(vec2 position)
+{
+    const vec2 magicVector = vec2(0.06711056, 0.00583715);
+    const float magicScalar = 52.9829189;
+    return fract(magicScalar * fract(dot(position, magicVector)));
+}
+
+vec2 vogelDisk(int index, float phi)
+{
+    const float goldenAngle = 2.39996322972865332;
+    float r = sqrt(index + 0.5) / sqrt(shadowSampleCount);
+    float theta = index * goldenAngle + phi;
+    return vec2(r * cos(theta), r * sin(theta));
+}
+
+vec2 findShadowOccluders(sampler2D shadowMap, vec3 coords, float receiver, float phi)
+{
+    float searchDistance = min(maxSearchDistance, lightSizeFactor / receiver * (receiver - nearPlane));
+    float scaledDistance = searchDistance * coords.z;
+    float depthSum = 0;
+    int occluderCount = 0;
+    for (int i = 0; i < shadowSampleCount; ++i)
+    {
+        vec3 offset = vec3(vogelDisk(i, phi) * scaledDistance, 0);
+        float depth = texture2DProj(shadowMap, coords + offset).r;
+        if (depth < receiver)
+        {
+            ++occluderCount;
+            depthSum += depth;
+        }
+    }
+    return vec2(depthSum / occluderCount, occluderCount);
+}
+
+float percentageCloserFilter(sampler2D shadowMap, vec3 coords, float receiver, float filterRadius, float phi)
+{
+    float scaledRadius = filterRadius * coords.z;
+    float sum = 0.0;
+    for (int i = 0; i < shadowSampleCount; ++i)
+    {
+        vec3 offset = vec3(vogelDisk(i, phi) * scaledRadius, 0);
+        sum += float(receiver <= texture2DProj(shadowMap, coords + offset).r);
+    }
+    return sum / shadowSampleCount;
+}
+
+float sampleShadow(sampler2D shadowMap, vec4 coords)
+{
+    float phi = interleavedGradientNoise(gl_FragCoord.xy);
+    float receiverDepth = min(coords.z / coords.w, 1);
+    vec3 coordsProj = coords.xyw;
+    vec2 occluders = findShadowOccluders(shadowMap, coordsProj, receiverDepth, phi);
+    if (occluders.y == 0)
+    {
+        return 1.0;
+    }
+
+    float meanDepth = occluders.x;
+    float penumbra = (receiverDepth - meanDepth) / meanDepth;
+    float filterRadius = penumbra * lightSizeFactor * nearPlane / receiverDepth;
+    filterRadius = min(filterRadius, maxFilterRadius);
+    return percentageCloserFilter(shadowMap, coordsProj, receiverDepth, filterRadius, phi);
+}
+#endif
 
 float unshadowedLightRatio(float distance)
 {
@@ -33,7 +105,7 @@ float unshadowedLightRatio(float distance)
 #endif
                 if (all(lessThan(shadowXYZ.xy, vec2(1.0, 1.0))) && all(greaterThan(shadowXYZ.xy, vec2(0.0, 0.0))))
                 {
-                    shadowing = min(shadow2DProj(shadowTexture@shadow_texture_unit_index, shadowSpaceCoords@shadow_texture_unit_index).r, shadowing);
+                    shadowing = min(sampleShadow(shadowTexture@shadow_texture_unit_index, shadowSpaceCoords@shadow_texture_unit_index), shadowing);
 
 
                     doneShadows = all(lessThan(shadowXYZ, vec3(0.95, 0.95, 1.0))) && all(greaterThan(shadowXYZ, vec3(0.05, 0.05, 0.0)));
@@ -45,7 +117,7 @@ float unshadowedLightRatio(float distance)
         @endforeach
     #else
         @foreach shadow_texture_unit_index @shadow_texture_unit_list
-            shadowing = min(shadow2DProj(shadowTexture@shadow_texture_unit_index, shadowSpaceCoords@shadow_texture_unit_index).r, shadowing);
+            shadowing = min(sampleShadow(shadowTexture@shadow_texture_unit_index, shadowSpaceCoords@shadow_texture_unit_index), shadowing);
         @endforeach
     #endif
 #if @limitShadowMapDistance
